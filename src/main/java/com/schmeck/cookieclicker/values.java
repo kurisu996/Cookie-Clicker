df801373017ac24fc26ab08b8910ca6dd90b11d6
Kurisu996 <72346331+kurisu996@users.noreply.github.com>
2024-08-16 20:52:47 +0200

    Initial commit

---
+++ src/main/java/com/schmeck/cookieclicker/values.java
@@ -0,0 +1,36 @@
+package com.schmeck.cookieclicker;
+
+public class values {
+    int cookieX, cookieY;
+    int cookieSize = 500;
+    int upgradeBoxX;
+    int cursorY;
+    int upgradeBoxSize = 150;
+    int upgradeBoxSize2 = 375;
+    int grandmaY;
+    boolean overCookie = false;
+    boolean overCursorBox = false;
+    boolean overGrandmaBox = false;
+    boolean overFarmBox = false;
+    boolean overMineBox = false;
+    boolean overFactoryBox = false;
+    boolean overMausBox = false;
+    boolean overBoostBox = false;
+    float geld = 0;
+    float Passiv = 0;
+    float passiv = 0;
+    float click = 1;
+    float boost = 1;
+    int cursorPreis = 15;
+    int grandmaPreis = 100;
+    int farmPreis = 1100;
+    int farmY;
+    int minePreis = 12000;
+    int mineY;
+    int factoryPreis = 130000;
+    int factoryY;
+    int boostersX;
+    int boostersY;
+    int boostPreis = 500;
+    int mausPreis = 100;
+}
